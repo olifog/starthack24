ef0308b24cc3c94e0e146b1a55b358e133530421
olifog <oliverfogelin@gmail.com>
2024-03-22 01:45:59 +0100

    WORKING VERSION UNO

--- src/server.py
+++ src/server.py
@@ -1,7 +1,5 @@
-import base64
-import json
-import logging
 import os
+from openai import OpenAI
 import twilio.jwt.access_token
 import twilio.jwt.access_token.grants
 import twilio.rest
@@ -10,6 +8,9 @@ from flask_sockets import Sockets
 from flask import Flask, request, Response
 from twilio.twiml.voice_response import VoiceResponse, Gather
 from dotenv import load_dotenv
+from query import query
+import json
+
 
 load_dotenv()
 
@@ -17,6 +18,11 @@ account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 api_key = os.environ["TWILIO_API_KEY_SID"]
 api_secret = os.environ["TWILIO_API_KEY_SECRET"]
 twilio_client = twilio.rest.Client(api_key, api_secret, account_sid)
+
+api_key = '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'
+client = OpenAI(api_key=api_key)
+
+system_prompt = open('src/system_prompt.txt', 'r').read()
 
 app = Flask(__name__)
 sockets = Sockets(app)
@@ -39,11 +45,9 @@ def start():
 
 @app.route("/process_speech", methods=['GET', 'POST'])
 def process_speech():
-    # Start our TwiML response
     resp = VoiceResponse()
 
-    # Start our <Gather> verb
-    gather = Gather(input='speech', language='de',speechTimeout=3, action='/gather_speech')
+    gather = Gather(input='speech', language='de-DE', speechTimeout=5, action='/gather_speech')
     resp.append(gather)
 
     return str(resp)
@@ -61,58 +65,26 @@ def gather_speech():
             resp.say('Auf Wiedersehen! Ich wünsche Ihnen einen schönen Tag in St. Gallen!', voice='Woman', language='de-DE')
             resp.redirect('/hangup')
         else:
-            resp.say("Ich habe nichts gehört. Bitte versuchen Sie es erneut.", voice='Woman', language='de-DE')
             resp.redirect('/process_speech')
     else:
-        # resp.play('https://demo.twilio.com/docs/classic.mp3')
-
-        resp.say('You said: {}'.format(speech_result), voice='Woman', language='de-DE')
+        documents = query(speech_result)
+        response = client.chat.completions.create(
+            max_tokens=1024,
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt + "\n\nInformation from the Kanton:" + json.dumps(documents),
+                },
+                {
+                    "role": "user",
+                    "content": speech_result
+                }
+            ],
+            model="gpt-4",
+        )        
+        resp.say(response.choices[0].message.content, voice='Woman', language='de-DE')
         resp.redirect('/process_speech')
     return str(resp)
 
-# @sockets.route('/media')
-# def echo(ws):
-#     app.logger.info("Connection accepted")
-#     # A lot of messages will be sent rapidly. We'll stop showing after the first one.
-#     has_seen_media = False
-#     message_count = 0
-#     while not ws.closed:
-#         message = ws.receive()
-#         if message is None:
-#             app.logger.info("No message received...")
-#             continue
-
-#         # Messages are a JSON encoded string
-#         data = json.loads(message)
-
-#         # Using the event type you can determine what type of message you are receiving
-#         if data['event'] == "connected":
-#             app.logger.info("Connected Message received: {}".format(message))
-#         if data['event'] == "start":
-#             app.logger.info("Start Message received: {}".format(message))
-#         if data['event'] == "media":
-#             if not has_seen_media:
-#                 app.logger.info("Media message: {}".format(message))
-#                 payload = data['media']['payload']
-#                 app.logger.info("Payload is: {}".format(payload))
-#                 chunk = base64.b64decode(payload)
-#                 app.logger.info("That's {} bytes".format(len(chunk)))
-#                 app.logger.info("Additional media messages from WebSocket are being suppressed....")
-#                 has_seen_media = True
-#         if data['event'] == "closed":
-#             app.logger.info("Closed Message received: {}".format(message))
-#             break
-#         message_count += 1
-
-#     app.logger.info("Connection closed. Received a total of {} messages".format(message_count))
-
-
 if __name__ == '__main__':
-    # app.logger.setLevel(logging.DEBUG)
-    # from gevent import pywsgi
-    # from geventwebsocket.handler import WebSocketHandler
-
-    # server = pywsgi.WSGIServer(('', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
-    # print("Server listening on: http://localhost:" + str(HTTP_SERVER_PORT))
-    # server.serve_forever()
     app.run(debug=True)
